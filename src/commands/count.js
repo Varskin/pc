c6e8367c7c01a9936ce0d8ea8343389ebb3b1d46
Super <38598824+Varskin@users.noreply.github.com>
2018-11-11 12:36:55 -0800

    Update count.js

--- src/commands/count.js
+++ src/commands/count.js
@@ -1,11 +1,4 @@
-const get = require('simple-get')
-
 exports.run = (message, args) => {
-  var playing
-  get.concat('http://games.roblox.com/v1/games/2487325459/servers/Public?sortOrder=Asc&limit=10', function (err, res, data) {
-  if (err) throw err
-  playing = data // Buffer('this is the server response')
-  })
   message.channel
     .send({ embed: { description: 'Pinging...' } })
     .then(newMessage => {
