d18d8127f6118f72d2d9cc7dea7ba8f3e82984ac
Super <38598824+Varskin@users.noreply.github.com>
2018-11-11 11:37:15 -0800

    Update count.js

--- src/commands/count.js
+++ src/commands/count.js
@@ -1,3 +1,17 @@
+
+function fetchNumPlayersInGame(placeUrl) {
+
+return new Promise((resolve, reject) => {
+    request(placeUrl, (err, response, body) => {
+        if (err) reject(err)
+        else {
+            let $ = cheerio.load(body)
+            resolve($('.game-stat .text-lead').first().text())
+        }
+    })
+}) 
+}
+
 exports.run = (message, args) => {
   message.channel
     .send({ embed: { description: 'Pinging...' } })
@@ -12,16 +26,3 @@ exports.run = (message, args) => {
     })
     .catch(error => console.log(error));
 };
-
-function fetchNumPlayersInGame(placeUrl) {
-
-return new Promise((resolve, reject) => {
-    request(placeUrl, (err, response, body) => {
-        if (err) reject(err)
-        else {
-            let $ = cheerio.load(body)
-            resolve($('.game-stat .text-lead').first().text())
-        }
-    })
-}) 
-}
